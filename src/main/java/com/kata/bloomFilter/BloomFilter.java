356a845dde96266c79eb1105bc4ab564bdcc9a63
bec <benoit.chiquet@gmail.com>
2015-05-20 01:06:22 +0200

    -Modified the binary chop test class for more readability. -Added the stub for the bloom filter kata.

---
+++ src/main/java/com/kata/bloomFilter/BloomFilter.java
@@ -0,0 +1,11 @@
+package com.kata.bloomFilter;
+
+import java.util.BitSet;
+
+/**
+ * Created by bec on 20/05/15.
+ */
+public class BloomFilter {
+
+    BitSet x;
+}
--- src/test/java/com/kata/chop/BinaryChopMethodsTest.java
+++ src/test/java/com/kata/chop/BinaryChopMethodsTest.java
@@ -14,26 +14,6 @@ public class BinaryChopMethodsTest {
     BinaryChopMethods binaryChopMethods = new BinaryChopMethods();
 
     //Tests for recursiveBinaryChop
-    @Test(expected = BinaryChopMethods.EmptyArrayException.class)
-    public void recursive_method_should_fail_when_array_is_empty() throws Exception {
-        binaryChopMethods.recursiveBinaryChop(1, new int[]{});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void recursive_method_should_fail_when_array_not_in_list_1() throws Exception {
-        binaryChopMethods.recursiveBinaryChop(4, new int[]{1, 2, 3});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void recursive_method_should_fail_when_array_not_in_list_2() throws Exception {
-        binaryChopMethods.recursiveBinaryChop(0, new int[]{1, 2, 3});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void recursive_method_should_fail_when_array_not_in_list_3() throws Exception {
-        binaryChopMethods.recursiveBinaryChop(3, new int[]{1, 2, 4});
-    }
-
     @Test
     public void recursive_method_should_return_correct_value() throws Exception {
         assertEquals(3, binaryChopMethods.recursiveBinaryChop(4, new int[]{1, 2, 3, 4, 6, 9, 12, 25, 30, 35, 40, 50, 100}));
@@ -42,7 +22,31 @@ public class BinaryChopMethodsTest {
         assertEquals(1, binaryChopMethods.recursiveBinaryChop(2, new int[]{1, 2}));
     }
 
+    @Test
+    public void recursive_method_should_fail_when_array_is_empty() throws Exception {
+        assertThatThrownBy(() -> binaryChopMethods.recursiveBinaryChop(1, new int[]{}))
+                .isInstanceOf(BinaryChopMethods.EmptyArrayException.class);
+    }
+
+    @Test
+    public void recursive_method_should_fail_when_value_not_in_array() throws Exception {
+        assertThatThrownBy(() -> binaryChopMethods.recursiveBinaryChop(4, new int[]{1, 2, 3}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
+        assertThatThrownBy(() -> binaryChopMethods.recursiveBinaryChop(0, new int[]{1, 2, 3}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
+        assertThatThrownBy(() -> binaryChopMethods.recursiveBinaryChop(3, new int[]{1, 2, 4}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
+    }
+
     //Tests for boundaryBinaryChop
+    @Test
+    public void boundary_method_should_return_correct_value() throws Exception {
+        assertEquals(3, binaryChopMethods.boundaryBinaryChop(4, new int[]{1, 2, 3, 4, 6, 9, 12, 25, 30, 35, 40, 50, 100}));
+        assertEquals(0, binaryChopMethods.boundaryBinaryChop(1, new int[]{1, 2, 3, 5, 8, 13, 21}));
+        assertEquals(6, binaryChopMethods.boundaryBinaryChop(21, new int[]{1, 2, 3, 5, 8, 13, 21}));
+        assertEquals(1, binaryChopMethods.boundaryBinaryChop(2, new int[]{1, 2}));
+    }
+
     @Test
     public void boundary_method_should_fail_when_array_is_empty() throws Exception {
         assertThatThrownBy(() -> binaryChopMethods.boundaryBinaryChop(1, new int[]{}))
@@ -50,7 +54,7 @@ public class BinaryChopMethodsTest {
     }
 
     @Test
-    public void boundary_method_should_fail_when_array_not_in_list() throws Exception {
+    public void boundary_method_should_fail_when_value_not_in_array() throws Exception {
         assertThatThrownBy(() -> binaryChopMethods.boundaryBinaryChop(4, new int[]{1, 2, 3}))
                 .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
         assertThatThrownBy(() -> binaryChopMethods.boundaryBinaryChop(0, new int[]{1, 2, 3}))
@@ -59,106 +63,54 @@ public class BinaryChopMethodsTest {
                 .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
     }
 
-    @Test
-    public void boundary_method_should_return_correct_value_1() throws Exception {
-        assertEquals(3, binaryChopMethods.boundaryBinaryChop(4, new int[]{1, 2, 3, 4, 6, 9, 12, 25, 30, 35, 40, 50, 100}));
-    }
-
-    @Test
-    public void boundary_method_should_return_correct_value_2() throws Exception {
-        assertEquals(0, binaryChopMethods.boundaryBinaryChop(1, new int[]{1, 2, 3, 5, 8, 13, 21}));
-    }
-
-    @Test
-    public void boundary_method_should_return_correct_value_3() throws Exception {
-        assertEquals(6, binaryChopMethods.boundaryBinaryChop(21, new int[]{1, 2, 3, 5, 8, 13, 21}));
-    }
-
-    @Test
-    public void boundary_method_should_return_correct_value_4() throws Exception {
-        assertEquals(1, binaryChopMethods.boundaryBinaryChop(2, new int[]{1, 2}));
-    }
-
     //Tests for reductionBinaryChop
-    @Test(expected = BinaryChopMethods.EmptyArrayException.class)
-    public void reduction_method_should_fail_when_array_is_empty() throws Exception {
-        binaryChopMethods.arrayReductionBinaryChop(1, new int[]{});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void reduction_method_should_fail_when_array_not_in_list_1() throws Exception {
-        binaryChopMethods.arrayReductionBinaryChop(4, new int[]{1, 2, 3});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void reduction_method_should_fail_when_array_not_in_list_2() throws Exception {
-        binaryChopMethods.arrayReductionBinaryChop(0, new int[]{1, 2, 3});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void reduction_method_should_fail_when_array_not_in_list_3() throws Exception {
-        binaryChopMethods.arrayReductionBinaryChop(3, new int[]{1, 2, 4});
-    }
-
     @Test
-    public void reduction_method_should_return_correct_value_1() throws Exception {
+    public void reduction_method_should_return_correct_value() throws Exception {
         assertEquals(3, binaryChopMethods.arrayReductionBinaryChop(4, new int[]{1, 2, 3, 4, 6, 9, 12, 25, 30, 35, 40, 50, 100}));
-    }
-
-    @Test
-    public void reduction_method_should_return_correct_value_2() throws Exception {
         assertEquals(0, binaryChopMethods.arrayReductionBinaryChop(1, new int[]{1, 2, 3, 5, 8, 13, 21}));
-    }
-
-    @Test
-    public void reduction_method_should_return_correct_value_3() throws Exception {
         assertEquals(6, binaryChopMethods.arrayReductionBinaryChop(21, new int[]{1, 2, 3, 5, 8, 13, 21}));
-    }
-
-    @Test
-    public void reduction_method_should_return_correct_value_4() throws Exception {
         assertEquals(1, binaryChopMethods.arrayReductionBinaryChop(2, new int[]{1, 2}));
     }
 
+    @Test
+    public void reduction_method_should_fail_when_array_is_empty() throws Exception {
+        assertThatThrownBy(() -> binaryChopMethods.arrayReductionBinaryChop(1, new int[]{}))
+                .isInstanceOf(BinaryChopMethods.EmptyArrayException.class);
+    }
+
+    @Test
+    public void reduction_method_should_fail_when_value_not_in_array() throws Exception {
+        assertThatThrownBy(()-> binaryChopMethods.arrayReductionBinaryChop(4, new int[]{1, 2, 3}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
+        assertThatThrownBy(() -> binaryChopMethods.arrayReductionBinaryChop(0, new int[]{1, 2, 3}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
+        assertThatThrownBy(() -> binaryChopMethods.arrayReductionBinaryChop(3, new int[]{1, 2, 4}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
+    }
+
     //Tests for movingViewBinaryChop
-    @Test(expected = BinaryChopMethods.EmptyArrayException.class)
-    public void view_method_should_fail_when_array_is_empty() throws Exception {
-        binaryChopMethods.movingViewBinaryChop(1, new int[]{});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void view_method_should_fail_when_array_not_in_list_1() throws Exception {
-        binaryChopMethods.movingViewBinaryChop(4, new int[]{1, 2, 3});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void view_method_should_fail_when_array_not_in_list_2() throws Exception {
-        binaryChopMethods.movingViewBinaryChop(0, new int[]{1, 2, 3});
-    }
-
-    @Test(expected = BinaryChopMethods.ValueNotInArrayException.class)
-    public void view_method_should_fail_when_array_not_in_list_3() throws Exception {
-        binaryChopMethods.movingViewBinaryChop(3, new int[]{1, 2, 4});
-    }
-
     @Test
-    public void view_method_should_return_correct_value_1() throws Exception {
+    public void view_method_should_return_correct_value() throws Exception {
         assertEquals(3, binaryChopMethods.movingViewBinaryChop(4, new int[]{1, 2, 3, 4, 6, 9, 12, 25, 30, 35, 40, 50, 100}));
-    }
-
-    @Test
-    public void view_method_should_return_correct_value_2() throws Exception {
         assertEquals(0, binaryChopMethods.movingViewBinaryChop(1, new int[]{1, 2, 3, 5, 8, 13, 21}));
-    }
-
-    @Test
-    public void view_method_should_return_correct_value_3() throws Exception {
         assertEquals(6, binaryChopMethods.movingViewBinaryChop(21, new int[]{1, 2, 3, 5, 8, 13, 21}));
+        assertEquals(1, binaryChopMethods.movingViewBinaryChop(2, new int[]{1, 2}));
     }
 
     @Test
-    public void view_method_should_return_correct_value_4() throws Exception {
-        assertEquals(1, binaryChopMethods.movingViewBinaryChop(2, new int[]{1, 2}));
+    public void view_method_should_fail_when_array_is_empty() throws Exception {
+        assertThatThrownBy(() -> binaryChopMethods.movingViewBinaryChop(1, new int[]{}))
+                .isInstanceOf(BinaryChopMethods.EmptyArrayException.class);
+    }
+
+    @Test
+    public void view_method_should_fail_when_value_not_in_array() throws Exception {
+        assertThatThrownBy(() -> binaryChopMethods.movingViewBinaryChop(4, new int[]{1, 2, 3}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
+        assertThatThrownBy(() -> binaryChopMethods.movingViewBinaryChop(0, new int[]{1, 2, 3}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
+        assertThatThrownBy(() -> binaryChopMethods.movingViewBinaryChop(3, new int[]{1, 2, 4}))
+                .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
     }
 
     //Test for libraryBinaryChop
@@ -177,7 +129,7 @@ public class BinaryChopMethodsTest {
     }
 
     @Test
-    public void library_method_should_fail_when_array_not_in_list() throws Exception {
+    public void library_method_should_fail_when_value_not_in_array() throws Exception {
         assertThatThrownBy(() -> binaryChopMethods.libraryBinaryChop(4, new int[]{1, 2, 3}))
                 .isInstanceOf(BinaryChopMethods.ValueNotInArrayException.class);
         assertThatThrownBy(() -> binaryChopMethods.libraryBinaryChop(0, new int[]{1, 2, 3}))
