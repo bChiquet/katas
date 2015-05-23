e15b90332694cb38b4937013861ca7cd233212fd
bec <benoit.chiquet@gmail.com>
2015-05-23 13:36:40 +0200

    Improved code coverage for the BloomFilter Changed the BloomFilter.addWord method so that words are added to the filter even when it is full. Cosmetic changes on the BloomFilter class.

--- src/main/java/com/kata/bloomFilter/BloomFilter.java
+++ src/main/java/com/kata/bloomFilter/BloomFilter.java
@@ -6,65 +6,108 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.BitSet;
-import java.util.PrimitiveIterator.OfInt;
+import java.util.Random;
+
 
 /**
  * Created by bec on 20/05/15.
  * Implements http://codekata.com/kata/kata05-bloom-filters/
  */
 public class BloomFilter {
+    //The bit array
+    private BitSet filter;
+    //The size of the bloom filter.
+    private int filterSize;
+    //The number of bits that are set to 1 for each word.
+    private int numberOfHashPerWord;
+    private int numberOfWordsStored;
+    private int expectedNumberOfElements;
 
-    static final int KEY = 7;
-    static final int MULT = 61;
+    /**
+     * Exception thrown when there is no filter size value that fits the couple
+     * (expectedElementNumber, acceptableFalsePositiveRate).
+     */
+    public class FilterCantBuildException extends Exception {}
 
-    BitSet filter;
-    long filterSize;
-
-    BloomFilter(int bitSize){
-        filter = new BitSet(bitSize);
-        filterSize = bitSize;
+    /**
+     * Constructs a simple bloom filter.
+     * The filter will automatically determine the bitmap size and the number of keys per word from :
+     * @param expectedNumberOfElements The number of words the bloom filter is expected to contain
+     * @param acceptableFalsePositiveRate The acceptable error rate when the filter is full.
+     * The calculation is explained here : http://pages.cs.wisc.edu/~cao/papers/summary-cache/node8.html
+     */
+    BloomFilter(int expectedNumberOfElements, double acceptableFalsePositiveRate) throws FilterCantBuildException {
+        int multiplier=0;
+        for (int i=2;i+1<Integer.MAX_VALUE/expectedNumberOfElements;i++){
+            if (Math.pow(0.6185, (double)i) < acceptableFalsePositiveRate){
+                multiplier = i+1;
+                break;
+            }
+        }
+        if (multiplier==0) {throw new FilterCantBuildException();}
+        numberOfHashPerWord = (int)(multiplier * Math.log(2));
+        filterSize = expectedNumberOfElements * multiplier;
+        filter = new BitSet(filterSize);
+        numberOfWordsStored = 0;
+        this.expectedNumberOfElements = expectedNumberOfElements;
     }
 
-    public void processWordFile(String filePath) {
+    /**
+     * adds all the words contained in a file in the bloom filter.
+     * The file format should be 1 word per line.
+     * @param filePath
+     * @return false if one or more words hasn't been added because of lack of space, true otherwise.
+     * The words will be added to the filter anyway
+     */
+    public boolean addWordsFromFile(String filePath) {
         try {
-            Files.lines(Paths.get(filePath)).forEach(this::addWordToFilter);
-        } catch (IOException e) {
+            return Files.lines(Paths.get(filePath))
+                    .map(this::addWord)
+                    .reduce((result, result2) -> result && result2)
+                    .get();
+        }
+        catch (IOException e) {
             Throwables.propagate(e);
+            return false;
         }
-
     }
 
-    public void addWordToFilter(String word) {
-        filter.or(getHash(word));
+    /**
+     * Adds a word's hash in the bloom filter.
+     * The word is trimmed before adding.
+     * @param word
+     * @return false if the word hasn't been added because the filter can't contain more words, true otherwise
+     * The word will be added to the filter anyway.
+     */
+    public boolean addWord(String word) {
+        filter.or(hash(word.trim()));
+        numberOfWordsStored++;
+        return numberOfWordsStored <= expectedNumberOfElements;
     }
 
-    public boolean isPresent(String word) {
-
-        BitSet hashOrFilter = getHash(word);
-        BitSet hashXorFilter = getHash(word);
+    /**
+     * checks if a word's hash is contained in the bloom filter.
+     * @param word
+     * @return true if the word's hash bits are all in the filter, false otherwise
+     */
+    public boolean contains(String word) {
+        BitSet hashOrFilter = hash(word);
         hashOrFilter.or(filter);
-        hashXorFilter.xor(filter);
-        return (hashXorFilter.cardinality() == filter.cardinality() - getHash(word).cardinality())
-                && (hashOrFilter.cardinality() == filter.cardinality());
+        return hashOrFilter.cardinality() == filter.cardinality();
     }
 
-    private BitSet getHash(String word){
-        //TODO The hash method makes no sense. Fix it !
-        long[] hash = new long[] {hashMethod1(word), hashMethod2(word)};
-        return BitSet.valueOf(hash);
+    /**
+     * @return the number of words that can be stored before the filter is full
+     */
+    public int spaceLeft(){
+        return expectedNumberOfElements - numberOfWordsStored;
     }
 
-    private long hashMethod1(String word) {
-        return 0;
-    }
-
-    private long hashMethod2(String word) {
-        OfInt wordCharIterator = word.chars().iterator();
-        int hash = KEY;
-        while (wordCharIterator.hasNext()) {
-            hash *= MULT;
-            hash += wordCharIterator.nextInt();
-        }
+    private BitSet hash(String word){
+        //This hash method sucks ; deal with it.
+        Random rand = new Random(word.hashCode());
+        BitSet hash = new BitSet(filterSize);
+        for (int i=0; i< numberOfHashPerWord;i++) {hash.set(rand.nextInt(filterSize), true);}
         return hash;
     }
 }
--- src/test/java/com/kata/bloomFilter/BloomFilterTest.java
+++ src/test/java/com/kata/bloomFilter/BloomFilterTest.java
@@ -4,10 +4,12 @@ import com.google.common.io.Resources;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by bec on 21/05/15.
+ * tests for class BloomFilter
  */
 public class BloomFilterTest {
 
@@ -15,38 +17,56 @@ public class BloomFilterTest {
     BloomFilter bloomFilter;
 
     @Before
-    public void setUp(){
-        bloomFilter = new BloomFilter(16*100000);
+    public void setUp() throws Exception {
+        int numberOfWords = 140000;
+        double falsePositiveRate = 0.0001;
+        bloomFilter = new BloomFilter(numberOfWords, falsePositiveRate);
+    }
+
+    @Test(expected = BloomFilter.FilterCantBuildException.class)
+    public void should_throw_with_unrealistic_filter_build_size() throws Exception{
+        new BloomFilter(Integer.MAX_VALUE/3, 0.0001);
+    }
+
+    @Test
+    public void should_return_false_when_too_many_words_are_added(){
+        bloomFilter.addWordsFromFile(Resources.getResource("wordlist").getPath());
+
+        boolean bulkWordAddCheck =
+                bloomFilter.addWordsFromFile(Resources.getResource("wordlist").getPath());
+        boolean wordAddCheck = bloomFilter.addWord("rot");
+
+        assertTrue(!bulkWordAddCheck);
+        assertTrue(!wordAddCheck);
     }
 
     @Test
     public void should_determine_if_word_hash_exists_in_bloomfilter(){
-        bloomFilter.addWordToFilter("art");
-        assertTrue(bloomFilter.isPresent("art"));
-        assertTrue(!bloomFilter.isPresent("arts"));
-        assertTrue(!bloomFilter.isPresent("ars"));
+        bloomFilter.addWord("art");
+        assertTrue(bloomFilter.contains("art"));
+        assertTrue(!bloomFilter.contains("arts"));
+        assertTrue(!bloomFilter.contains("ars"));
     }
 
     @Test
     public void should_process_word_list(){
-        bloomFilter.processWordFile(Resources.getResource("wordlist").getPath());
-        assertTrue(bloomFilter.isPresent("artisan"));
+        bloomFilter.addWordsFromFile(Resources.getResource("wordlist").getPath());
+        assertTrue(bloomFilter.contains("artisan"));
+        assertEquals(281, bloomFilter.spaceLeft());
     }
 
     @Test
     public void should_add_word_to_bitset(){
-        bloomFilter.addWordToFilter("craftsman");
-        assertTrue(bloomFilter.isPresent("craftsman"));
+        bloomFilter.addWord("craftsman");
+        assertTrue(bloomFilter.contains("craftsman"));
+        assertEquals(139999, bloomFilter.spaceLeft());
     }
 
     @Test
-    public void should_have_low_false_positive_frequeny() {
-        bloomFilter.processWordFile(Resources.getResource("wordlist").getPath());
+    public void should_have_acceptable_false_positive_frequeny() {
+        bloomFilter.addWordsFromFile(Resources.getResource("wordlist").getPath());
         //TODO generate a couple thousand imaginary words not in wordlist and check if
         // bloomfilter says they exist. Frequency should be inferior to :
         System.out.println(ACCEPTABLE_FALSE_POSITIVE_FREQUENCY);
     }
-
-
-
 }
